ccc39253f3becd7c642157d8af11a2c4ab92c004
Ilya Cherney <ilyacherney@yandex.ru>
2024-11-13 12:26:04 +0300

    Update README.md, delete unnecessary comments

--- src/main/java/ru/otus/s3/service/Application.java
+++ src/main/java/ru/otus/s3/service/Application.java
@@ -1,11 +1,6 @@
 package ru.otus.s3.service;
 
 public class Application {
-    // Домашнее задание:
-    // 1. Избавиться от sout'ов и подключить логгер
-    // 2. Сделайте парсинг заголовков запросов в Map<String, String>
-    // 3. * Добавьте обработку 405 Method Not Allowed
-
     public static void main(String[] args) {
         new HttpServer(8190).start();
     }
